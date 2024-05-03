fe030a0a869d792fe7297c001266ffb368c7490d
MattiaSilvestri <mattiasilvestri27@gmail.com>
2024-05-03 11:31:08 +0200

    show list of suggestions in textual

--- src/mv-suggest.py
+++ src/mv-suggest.py
@@ -1,17 +1,12 @@
 import utils.widgets as widgets
-import utils.helper as helper
-from textual.app import App, ComposeResult
-from textual.widgets import Button, Footer, Header, Static
-from textual.containers import ScrollableContainer
-from textual.reactive import reactive
 
-# # Get movie title from user
-# title = widgets.get_titles()
-#
-# # Provide suggestions
-# suggestions = helper.lookup_title("../data/movie_similarity.csv", title, 10)
-# for i in suggestions["index"].values:
-#     print(i)
+# import utils.helper as helper
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, ListItem, ListView, Label, Input
+from textual.containers import ScrollableContainer
+from utils import helper
+
+# from textual.reactive import reactive
 
 
 class MvSuggest(App):
@@ -20,11 +15,29 @@ class MvSuggest(App):
     """
 
     CSS_PATH = "./tcss/styles.tcss"
+    BINDINGS = []
 
     def compose(self) -> ComposeResult:
         """Create child widgets of the app."""
         yield Header()
-        yield ScrollableContainer(widgets.TextInput("text"))
+        yield Footer()
+        yield ScrollableContainer(Input("text"), id="list")
+
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        """Handle submit action."""
+
+        movie_query = event.input.value
+        suggestions_df = helper.lookup_title(
+            "../data/movie_similarity.csv", movie_query, 10
+        )
+
+        for i in suggestions_df["index"].values:
+            print(i)
+
+        suggestions = [ListItem(Label(i))
+                       for i in suggestions_df["index"].values]
+        new_list = ListView(*suggestions)
+        self.query_one("#list").mount(new_list)
 
 
 if __name__ == "__main__":
